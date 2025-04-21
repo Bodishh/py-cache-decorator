2c0b16626793fda12eb14a9312a0ceafe195e1d4
Bohdan <bsuhij@gmail.com>
2025-04-21 15:06:39 +0300

    Task done

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache = {}
+
+    def innit(*args, **kwargs) -> Any:
+        key = (args, tuple(sorted(kwargs.items())))
+
+        if key in cache:
+            print("Getting from cache")
+            return cache[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache[key] = result
+            return result
+
+    return innit
